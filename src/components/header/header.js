8b4363f0daeb4353207b28d5b10ec0efa3fb9e28
Jack Varble <jackvarble@gmail.com>
2022-08-25 16:33:57 -0500

    add global styles

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./header.scss";
 
 const header = () => {
-  return <div>header</div>;
+  return (
+    <header>
+      <div className="container header-container">
+        <h5>Hello I am</h5>
+        <h1>Jack Varble</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+      </div>
+    </header>
+  );
 };
 
 export default header;
