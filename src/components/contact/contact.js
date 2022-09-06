c71e0c88404b982f35f53091f6831b6132226336
JackVarble <jackvarble@gmail.com>
2022-09-06 18:55:56 -0500

    fix contact placeholders

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -37,7 +37,7 @@ const Contact = () => {
           <input
             name="email"
             type="email"
-            placeholder="Email address"
+            placeholder="Email Address"
             onChange={onChange}
             required
           />
